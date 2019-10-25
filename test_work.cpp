2c22b4b5bba67b0dc21deccbf6a62650593f3227
Yaroslav Loshkarev <56892972+Yaroslav-Loshkarev@users.noreply.github.com>
2019-10-25 15:12:11 +0300

    Update test_work.cpp

--- test_work.cpp
+++ test_work.cpp
@@ -4,44 +4,45 @@
 #include <ctime>
 
 using namespace std;
-
-//функция получения индекса максимального элемента
+/*
+*функция получения индекса максимального элемента
+*/
 int get_index_of_max_array_value(vector<int> your_array)
 {
-	
 	srand(time(0));
-// заполнение массива случайными числами
-	cout<<"Your array with random elements:";
-	
+/*
+*заполнение массива случайными числами
+*/
+	cout<<"Your array with random elements:";	
 	for (int i=0;i<your_array.size();i++)
 	{
 		your_array.at(i)=(rand() % 200);
 			cout<<your_array[i]<<" ";
-	}
-	
+	}	
 	cout<<endl;cout<<endl;	
-//______________________________________________	
-		
+/*
+*инициализация переменной для хранения максимального значения
+*инициализация индекса максимального элемента
+*/
 	int max_value=your_array[0];	
-	int index_of_max_value=0;
-	
-//определение индекса максимального элемента массива
+	int index_of_max_value=0;	
+/*
+*определение индекса максимального элемента массива
+*/
 	for(int i=0;i<your_array.size();i++)
 	{
 		if(your_array[i]>=max_value)
 		{
 			max_value=your_array[i];
-			index_of_max_value=i;
-			
-		}
-	
+			index_of_max_value=i;			
+		}	
 	}	
 	return index_of_max_value;
 }
-
-
 int main(){
-	//результат
+	/*
+	*результат
+	*/
 	vector <int> test_array(10);	
 		cout<<"Index of the biggest value:"<<get_index_of_max_array_value(test_array);	
 	return 0;
